bb531b0846cb17156f91f255f56e7af58efa3d61
pavlelukic <pavlelukic2015@gmail.com>
2025-09-03 00:23:14 +0200

    Created rename_images.py which renames all of the images in an uniform way. {vehicle_type}_XXXXX.jpg

---
+++ scripts/rename_images.py
@@ -0,0 +1,28 @@
+import os
+
+ROOT = "dataset_updated"
+
+for label in os.listdir(ROOT):
+    label_dir = os.path.join(ROOT, label)
+
+    if not os.path.isdir(label_dir):
+        continue
+
+    count = 1
+
+    for fname in sorted(os.listdir(label_dir)):
+        src = os.path.join(label_dir, fname)
+
+        if not os.path.isfile(src):
+            continue
+
+        if not fname.lower().endswith((".jpg", ".jpeg", ".png")):
+            continue
+
+        dst = os.path.join(label_dir, f"{label}_{count:05d}.jpg")
+        if src != dst:
+            os.rename(src, dst)
+
+        count += 1
+
+print("Images have been renamed successfully!")
