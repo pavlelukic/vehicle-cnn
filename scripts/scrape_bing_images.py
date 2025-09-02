2a7c2614de377c14b37c79948b65f779fb614991
pavlelukic <pavlelukic2015@gmail.com>
2025-09-02 21:29:27 +0200

    Tweaked the logic of the script, and set the limit to 30 for testing purposes.

--- scripts/scrape_bing_images.py
+++ scripts/scrape_bing_images.py
@@ -11,20 +11,19 @@ queries = {
 for label, query in queries.items():
     downloader.download(
         query=query,
-        limit=500,
+        limit=100,
         output_dir='dataset',
         adult_filter_off=True,
         timeout=60,
         verbose=True
     )
 
-    src = os.path.join('dataset', query.replace(" ", "_"))
+    src = os.path.join('dataset', query)
     dst = os.path.join('dataset', label)
     os.makedirs(dst, exist_ok=True)
 
     for fname in os.listdir(src):
-        path = os.path.join(src, fname)
-        if os.path.isfile(path):
-            os.replace(path, os.path.join(dst, fname))
+        os.replace(os.path.join(src, fname),
+                   os.path.join(dst, fname))
 
     os.rmdir(src)
